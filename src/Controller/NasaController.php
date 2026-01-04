6610afe0a04a46335c42155413f4fd02fcc9c104
Valentin AZANCOTH <val.zancoth@gmail.com>
2026-01-04 15:14:02 +0100

    feat(controller): Link NasaController with NasaApiService
    - Install Monolog

--- src/Controller/NasaController.php
+++ src/Controller/NasaController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Service\NasaApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,12 +12,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class NasaController extends AbstractController
 {
     #[Route('/', name: 'app_nasa')]
-    public function index(): Response
+    public function index(NasaApiService $nasaApiService): Response
     {
+        $data = $nasaApiService->fetchNasaAPI();
+
         return $this->render('nasa/index.html.twig', [
-                'title' => 'Image du jour',
-                'url' => 'https://static.nationalgeographic.fr/files/styles/image_3200/public/ouverture-espace-une-nouvelle-ere-de-decouvertes-ng289.webp',
-                'explanation' => 'Description de l\'image',
+                'title' => $data['title'],
+                'url' => $data['url'],
+                'explanation' => $data['explanation'],
             ]
         );
     }
